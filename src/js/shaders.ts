0d8186bb74a4d005c7090fe178bb6a910f60be45
Sovch <valentinafediakova@gmail.com>
2025-06-09 20:43:57 +0400

    shaders

--- src/js/shaders.ts
+++ src/js/shaders.ts
@@ -1,57 +1,69 @@
-export const vertexShaderDeform = `
+export const vertexShaderPlane = `
   varying vec2 vUv;
   uniform vec3 uDisplacement;
-  uniform float uMinDistance;
-  uniform float uScale;
-
+  
   float easeInOutCubic(float x) {
-    return x < 0.5 
-      ? 4.0 * x * x * x 
-      : 1.0 - pow(-2.0 * x + 2.0, 3.0) / 2.0;
+    return x < 0.5
+      ? 4. * x * x * x
+      : 1. - pow(-2. * x + 2., 3.) / 2.;
   }
 
-  float map(float v, float a1, float a2, float b1, float b2) {
-    return b1 + (v - a1) * (b2 - b1) / (a2 - a1);
-  }
+  float map(float value, float min1, float max1, float min2, float max2) {
+    return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+  }  
 
   void main() {
     vUv = uv;
-    vec3 pos = position;
-    vec4 worldPos = modelMatrix * vec4(position, 1.0);
-    float dist = length(uDisplacement - worldPos.xyz);
-
-    if (dist < uMinDistance) {
-      float t = map(dist, 0.0, uMinDistance, 1.0, 0.0);
-      pos.z += easeInOutCubic(t) * uScale;
+    vec3 new_position = position;
+    vec4 worldPosition = modelMatrix * vec4(position, 1.);
+    float dist = length(uDisplacement - worldPosition.rgb);
+    float min_distance = 3.;
+    if (dist < min_distance) {
+      float t = map(dist, 0., min_distance, 1., 0.);
+      new_position.z += easeInOutCubic(t) * 1.;
     }
-
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(new_position, 1.0);
   }
 `;
 
-export const fragmentShaderDeform = `
+export const fragmentShaderPlane = `
   varying vec2 vUv;
-  uniform vec3 uColor;
-
+  uniform sampler2D uTexture;
   void main() {
-    gl_FragColor = vec4(uColor, 1.0);
+    vec4 color = texture2D(uTexture, vUv);
+    gl_FragColor = vec4(color);
   }
 `;
 
 export const vertexShaderShadow = `
   varying vec2 vUv;
+  varying float dist;
+  uniform vec3 uDisplacement;
+
   void main() {
     vUv = uv;
+    vec4 worldPosition = modelMatrix * vec4(position, 1.);
+    dist = length(uDisplacement - worldPosition.rgb);
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
   }
 `;
 
 export const fragmentShaderShadow = `
   varying vec2 vUv;
+  varying float dist;
   uniform sampler2D uTexture;
 
+  float map(float value, float min1, float max1, float min2, float max2) {
+    return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+  }
+
   void main() {
-    vec4 tex = texture2D(uTexture, vUv);
-    gl_FragColor = vec4(vec3(tex.r), tex.a);
+    vec4 color = texture2D(uTexture, vUv);
+    float min_distance = 3.;
+    if (dist < min_distance) {
+      float alpha = map(dist, min_distance, 0., color.a, 0.);
+      color.a = alpha;
+    }
+    gl_FragColor = vec4(color);
   }
 `;
