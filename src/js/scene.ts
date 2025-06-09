ea4714e872ad17ca9895ffa4afac11093a0b7a4a
Sovch <valentinafediakova@gmail.com>
2025-06-09 20:49:19 +0400

    WIP

--- src/js/scene.ts
+++ src/js/scene.ts
@@ -1,37 +1,32 @@
 import * as THREE from "three";
 
-let aspect = window.innerWidth / window.innerHeight;
-const camera_distance = 8;
+export let aspect = window.innerWidth / window.innerHeight;
+export const cameraDistance = 8;
 
 export const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 
 export const camera = new THREE.OrthographicCamera(
-  -camera_distance * aspect,
-  camera_distance * aspect,
-  camera_distance,
-  -camera_distance,
+  -cameraDistance * aspect,
+  cameraDistance * aspect,
+  cameraDistance,
+  -cameraDistance,
   0.01,
   1000
 );
-camera.position.set(0, -10, 5);
+camera.position.set(-0, -10, 5);
 camera.lookAt(0, 0, 0);
 
 export const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(new THREE.Color(0x000000));
-
 document.body.appendChild(renderer.domElement);
 
-export let currentAspect = aspect;
-export const CAMERA_DISTANCE = camera_distance;
-
-export function resizeRenderer() {
-  currentAspect = window.innerWidth / window.innerHeight;
-  camera.left = -CAMERA_DISTANCE * currentAspect;
-  camera.right = CAMERA_DISTANCE * currentAspect;
-  camera.top = CAMERA_DISTANCE;
-  camera.bottom = -CAMERA_DISTANCE;
-
+export function onWindowResize() {
+  aspect = window.innerWidth / window.innerHeight;
+  camera.left = -cameraDistance * aspect;
+  camera.right = cameraDistance * aspect;
+  camera.top = cameraDistance;
+  camera.bottom = -cameraDistance;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
