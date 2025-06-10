d3362a6a55503526a2d5c7676d73675ebab8feae
Sovch <valentinafediakova@gmail.com>
2025-06-10 19:40:16 +0400

    add controls

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { scene } from "./js/scene";
+import { scene, renderer } from "./js/scene";
 import { sphere, hitPlane, plane, planeShadow } from "./js/objects";
 import { setupEventListeners } from "./js/events";
 import { animate } from "./js/animate";
+import * as THREE from "three";
 
 scene.add(sphere);
 scene.add(hitPlane);
@@ -11,3 +12,25 @@ scene.add(plane);
 setupEventListeners();
 
 animate();
+
+const spherePlusButton = document.getElementById("sphere-plus");
+if (spherePlusButton) {
+  spherePlusButton.addEventListener("click", () => {
+    sphere.scale.multiplyScalar(1.2);
+  });
+}
+const sphereMinusButton = document.getElementById("sphere-minus");
+if (sphereMinusButton) {
+  sphereMinusButton.addEventListener("click", () => {
+    sphere.scale.multiplyScalar(0.8);
+  });
+}
+
+const textColorInput = document.getElementById("text-color");
+if (textColorInput) {
+  textColorInput.addEventListener("input", (e) => {
+    const hex = (e.target as HTMLInputElement).value;
+    const color = new THREE.Color(hex);
+    plane.material.uniforms.uTextColor.value.copy(color);
+  });
+}
